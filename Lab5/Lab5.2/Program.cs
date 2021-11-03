ccfe2e63d7d243578867ba64f706c667e3f8aee2
Borysman <bormaxv@gmail.com>
2021-11-03 23:45:59 +0200

    Co-authored-by: TheValik <TheValik@users.noreply.github.com>

---
+++ Lab5/Lab5.2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+using System.Diagnostics;
+
+namespace Lab5._2
+{
+    public class PBKDF2
+    {
+        public static byte[] GenerateSalt()
+        {
+            using (var randomNumberGenerator = new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[32];
+                randomNumberGenerator.GetBytes(randomNumber);
+                return randomNumber;
+            }
+        }
+        public static byte[] HashPassword(byte[] toBeHashed, byte[] salt, int numberOfRounds, System.Security.Cryptography.HashAlgorithmName hashAlgorithm, Int32 NumberOfBytes)
+        {
+            using (var rfc2898 = new Rfc2898DeriveBytes(toBeHashed, salt, numberOfRounds, hashAlgorithm))
+            {
+                return rfc2898.GetBytes(NumberOfBytes);
+            }
+        }
+}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string passwordToHash = "VeryComplexPassword";
+            Console.WriteLine("Algoritm SHA1");
+            HashPassword(passwordToHash, 30000);
+            HashPassword(passwordToHash, 80000);
+            HashPassword(passwordToHash, 130000);
+            HashPassword(passwordToHash, 180000);
+            HashPassword(passwordToHash, 230000);
+            HashPassword(passwordToHash, 280000);
+            HashPassword(passwordToHash, 330000);
+            HashPassword(passwordToHash, 380000);
+            HashPassword(passwordToHash, 430000);
+            HashPassword(passwordToHash, 480000);
+            // Console.WriteLine(Rfc2898DeriveBytes.HashAlgorithm);
+        }
+        public static void HashPassword(string passwordToHash, int numberOfRounds)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            var hashedPassword = PBKDF2.HashPassword(Encoding.UTF8.GetBytes(passwordToHash), PBKDF2.GenerateSalt(), numberOfRounds, HashAlgorithmName.SHA1, 20);
+            sw.Stop();
+            Console.WriteLine();
+            Console.WriteLine("Password to hash : " + passwordToHash);
+            Console.WriteLine("Hashed Password : " + Convert.ToBase64String(hashedPassword));
+            Console.WriteLine("Iterations <" + numberOfRounds + "> Elapsed Time: " + sw.ElapsedMilliseconds + "ms");
+        }
+    }
+
+}
+
---
+++ Lab5/Lab5.3/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+using System.Collections.Generic;
+
+namespace _4part
+{
+    public class SaltedHash
+    {
+        public static byte[] GenerateSalt()
+        {
+            const int saltLength = 32;
+            using (var randomNumberGenerator = new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[saltLength];
+                randomNumberGenerator.GetBytes(randomNumber);
+                return randomNumber;
+            }
+        }
+        private static byte[] Combine(byte[] first, byte[] second)
+        {
+            var ret = new byte[first.Length + second.Length];
+            Buffer.BlockCopy(first, 0, ret, 0, first.Length);
+            Buffer.BlockCopy(second, 0, ret, first.Length,second.Length);
+            return ret;
+        }
+        public static byte[] HashPasswordWithSalt(byte[] toBeHashed, byte[] salt)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                return sha256.ComputeHash(Combine(toBeHashed,salt));
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> logins = new List<string> ();
+            List<string> passwords = new List<string> ();
+            List<string> salts = new List<string> ();
+            while (true)
+            {
+
+                Console.Write("Enter '1' to logIN and '0' to Register: ");
+                var temp = Console.ReadLine();
+                if (temp == "1")
+                {
+                    Console.Write("Please enter your login: ");
+                    var login = Console.ReadLine();
+                    if (logins.Contains(login)){
+                        var ind = logins.IndexOf(login);
+                        Console.Write("Please enter your password: ");
+                        var pass = Console.ReadLine();
+                        var salt = Convert.FromBase64String(salts[ind]);
+                        var sha256Forpass = Convert.ToBase64String(SaltedHash.HashPasswordWithSalt(Encoding.Unicode.GetBytes(pass), salt));
+                        if (passwords[ind] == sha256Forpass){
+                            Console.WriteLine("You loggined");
+                        } else
+                        {
+                            Console.WriteLine("Wrong password");
+                        }
+                        
+                    } else
+                    {
+                        Console.WriteLine("Wrong login");
+                    }
+                }else if (temp == "0"){
+                    Console.Write("Please enter your login: ");
+                    var login = Console.ReadLine();
+                    if (!logins.Contains(login)){
+                        logins.Add(login);
+                        Console.Write("Please enter your password: ");
+                        var pass = Console.ReadLine();
+                        var salt = SaltedHash.GenerateSalt();
+                        var sha256Forpass = Convert.ToBase64String(SaltedHash.HashPasswordWithSalt(Encoding.Unicode.GetBytes(pass), salt));
+                        passwords.Add(sha256Forpass);
+                        salts.Add(Convert.ToBase64String(salt));
+                        Console.WriteLine("you registered");
+                        
+                    } else
+                    {
+                        Console.WriteLine("login already in base");
+                        continue;
+                    }
+                } else
+                {
+                   Console.WriteLine("Enter correct please"); 
+                }
+            }
+        }
+    }
+}
