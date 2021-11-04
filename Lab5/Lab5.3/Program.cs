baeee1f0e4285dbc3da16be2cb88b54506d7d891
Borysman <bormaxv@gmail.com>
2021-11-04 13:42:01 +0200

    fix

--- Lab5/Lab5.3/Program.cs
+++ Lab5/Lab5.3/Program.cs
@@ -5,30 +5,22 @@ using System.Collections.Generic;
 
 namespace _4part
 {
-    public class SaltedHash
+    public class PBKDF2
     {
         public static byte[] GenerateSalt()
         {
-            const int saltLength = 32;
             using (var randomNumberGenerator = new RNGCryptoServiceProvider())
             {
-                var randomNumber = new byte[saltLength];
+                var randomNumber = new byte[32];
                 randomNumberGenerator.GetBytes(randomNumber);
                 return randomNumber;
             }
         }
-        private static byte[] Combine(byte[] first, byte[] second)
+        public static byte[] HashPassword(byte[] toBeHashed, byte[] salt, int numberOfRounds, System.Security.Cryptography.HashAlgorithmName hashAlgorithm, Int32 NumberOfBytes)
         {
-            var ret = new byte[first.Length + second.Length];
-            Buffer.BlockCopy(first, 0, ret, 0, first.Length);
-            Buffer.BlockCopy(second, 0, ret, first.Length,second.Length);
-            return ret;
-        }
-        public static byte[] HashPasswordWithSalt(byte[] toBeHashed, byte[] salt)
-        {
-            using (var sha256 = SHA256.Create())
+            using (var rfc2898 = new Rfc2898DeriveBytes(toBeHashed, salt, numberOfRounds, hashAlgorithm))
             {
-                return sha256.ComputeHash(Combine(toBeHashed,salt));
+                return rfc2898.GetBytes(NumberOfBytes);
             }
         }
     }
@@ -36,9 +28,9 @@ namespace _4part
     {
         static void Main(string[] args)
         {
-            List<string> logins = new List<string> ();
-            List<string> passwords = new List<string> ();
-            List<string> salts = new List<string> ();
+            List<string> logins = new List<string>();
+            List<string> passwords = new List<string>();
+            List<string> salts = new List<string>();
             while (true)
             {
 
@@ -48,44 +40,53 @@ namespace _4part
                 {
                     Console.Write("Please enter your login: ");
                     var login = Console.ReadLine();
-                    if (logins.Contains(login)){
+                    if (logins.Contains(login))
+                    {
                         var ind = logins.IndexOf(login);
                         Console.Write("Please enter your password: ");
                         var pass = Console.ReadLine();
                         var salt = Convert.FromBase64String(salts[ind]);
-                        var sha256Forpass = Convert.ToBase64String(SaltedHash.HashPasswordWithSalt(Encoding.Unicode.GetBytes(pass), salt));
-                        if (passwords[ind] == sha256Forpass){
+                        var sha256Forpass = Convert.ToBase64String(PBKDF2.HashPassword(Encoding.Unicode.GetBytes(pass), salt, 30000, HashAlgorithmName.SHA256, 32));
+                        if (passwords[ind] == sha256Forpass)
+                        {
                             Console.WriteLine("You loggined");
-                        } else
+                        }
+                        else
                         {
                             Console.WriteLine("Wrong password");
                         }
-                        
-                    } else
+
+                    }
+                    else
                     {
                         Console.WriteLine("Wrong login");
                     }
-                }else if (temp == "0"){
+                }
+                else if (temp == "0")
+                {
                     Console.Write("Please enter your login: ");
                     var login = Console.ReadLine();
-                    if (!logins.Contains(login)){
+                    if (!logins.Contains(login))
+                    {
                         logins.Add(login);
                         Console.Write("Please enter your password: ");
                         var pass = Console.ReadLine();
-                        var salt = SaltedHash.GenerateSalt();
-                        var sha256Forpass = Convert.ToBase64String(SaltedHash.HashPasswordWithSalt(Encoding.Unicode.GetBytes(pass), salt));
+                        var salt = PBKDF2.GenerateSalt();
+                        var sha256Forpass = Convert.ToBase64String(PBKDF2.HashPassword(Encoding.Unicode.GetBytes(pass), salt, 30000, HashAlgorithmName.SHA256, 32));
                         passwords.Add(sha256Forpass);
                         salts.Add(Convert.ToBase64String(salt));
                         Console.WriteLine("you registered");
-                        
-                    } else
+
+                    }
+                    else
                     {
                         Console.WriteLine("login already in base");
                         continue;
                     }
-                } else
+                }
+                else
                 {
-                   Console.WriteLine("Enter correct please"); 
+                    Console.WriteLine("Enter correct please");
                 }
             }
         }
