a82d5d4c481b05797e6719c412bf6de134c39810
Borysman <bormaxv@gmail.com>
2021-11-06 17:20:43 +0200

    дфи 6

---
+++ Lab6/Lab6.1/Program.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Security.Cryptography;
+using System.Collections.Generic;
+
+namespace Lab6._1
+{
+    class desChipher
+    {
+        public static byte[] Encrypt(byte[] dataToEncrypt, byte[] key, byte[] iv)
+        {
+            using (var des = new DESCryptoServiceProvider())
+            {
+                des.Mode = CipherMode.CBC;
+                des.Padding = PaddingMode.PKCS7;
+                des.Key = key;
+                des.IV = iv;
+                using (var memoryStream = new MemoryStream())
+                {
+                    var cryptoStream = new CryptoStream(memoryStream, des.CreateEncryptor(), CryptoStreamMode.Write);
+                    cryptoStream.Write(dataToEncrypt, 0, dataToEncrypt.Length);
+                    cryptoStream.FlushFinalBlock();
+                    return memoryStream.ToArray();
+                }
+            }
+        }
+        public static byte[] Decrypt(byte[] dataToDecrypt, byte[] key, byte[] iv)
+        {
+            using (var des = new DESCryptoServiceProvider())
+            {
+                des.Mode = CipherMode.CBC;
+                des.Padding = PaddingMode.PKCS7;
+                des.Key = key;
+                des.IV = iv;
+                using (var memoryStream = new MemoryStream())
+                {
+                    var cryptoStream = new CryptoStream(memoryStream, des.CreateDecryptor(), CryptoStreamMode.Write);
+                    cryptoStream.Write(dataToDecrypt, 0, dataToDecrypt.Length);
+                    cryptoStream.FlushFinalBlock();
+                    return memoryStream.ToArray();
+                }
+            }
+        }
+    }
+    class TripledesChipher
+    {
+        public static byte[] Encrypt(byte[] dataToEncrypt, byte[] key, byte[] iv)
+        {
+            using (var des = new TripleDESCryptoServiceProvider())
+            {
+                des.Mode = CipherMode.CBC;
+                des.Padding = PaddingMode.PKCS7;
+                des.Key = key;
+                des.IV = iv;
+                using (var memoryStream = new MemoryStream())
+                {
+                    var cryptoStream = new CryptoStream(memoryStream, des.CreateEncryptor(), CryptoStreamMode.Write);
+                    cryptoStream.Write(dataToEncrypt, 0, dataToEncrypt.Length);
+                    cryptoStream.FlushFinalBlock();
+                    return memoryStream.ToArray();
+                }
+            }
+        }
+        public static byte[] Decrypt(byte[] dataToDecrypt, byte[] key, byte[] iv)
+        {
+            using (var des = new TripleDESCryptoServiceProvider())
+            {
+                des.Mode = CipherMode.CBC;
+                des.Padding = PaddingMode.PKCS7;
+                des.Key = key;
+                des.IV = iv;
+                using (var memoryStream = new MemoryStream())
+                {
+                    var cryptoStream = new CryptoStream(memoryStream, des.CreateDecryptor(), CryptoStreamMode.Write);
+                    cryptoStream.Write(dataToDecrypt, 0, dataToDecrypt.Length);
+                    cryptoStream.FlushFinalBlock();
+                    return memoryStream.ToArray();
+                }
+            }
+        }
+    }
+    class aesChipher
+    {
+        public static byte[] GenerateRandomNumber(int length)
+        {
+            using (var randomNumberGenerator = new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[length];
+                randomNumberGenerator.GetBytes(randomNumber);
+                return randomNumber;
+            }
+        }
+        public static byte[] Encrypt(byte[] dataToEncrypt, byte[] key, byte[] iv)
+        {
+            using (var aes = new AesCryptoServiceProvider())
+            {
+                aes.Mode = CipherMode.CBC;
+                aes.Padding = PaddingMode.PKCS7;
+                aes.Key = key;
+                aes.IV = iv;
+                using (var memoryStream = new MemoryStream())
+                {
+                    var cryptoStream = new CryptoStream(memoryStream, aes.CreateEncryptor(), CryptoStreamMode.Write);
+                    cryptoStream.Write(dataToEncrypt, 0, dataToEncrypt.Length);
+                    cryptoStream.FlushFinalBlock();
+                    return memoryStream.ToArray();
+                }
+            }
+        }
+        public static byte[] Decrypt(byte[] dataToDecrypt, byte[] key, byte[] iv)
+        {
+            using (var aes = new AesCryptoServiceProvider())
+            {
+                aes.Mode = CipherMode.CBC;
+                aes.Padding = PaddingMode.PKCS7;
+                aes.Key = key;
+                aes.IV = iv;
+                using (var memoryStream = new MemoryStream())
+                {
+                    var cryptoStream = new CryptoStream(memoryStream, aes.CreateDecryptor(), CryptoStreamMode.Write);
+                    cryptoStream.Write(dataToDecrypt, 0, dataToDecrypt.Length);
+                    cryptoStream.FlushFinalBlock();
+                    return memoryStream.ToArray();
+                }
+            }
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var key = aesChipher.GenerateRandomNumber(8);
+            var iv = aesChipher.GenerateRandomNumber(8);
+            const string original = "Text to encrypt 1";
+            var encrypted = desChipher.Encrypt(Encoding.UTF8.GetBytes(original), key, iv);
+            var decrypted = desChipher.Decrypt(encrypted, key, iv);
+            var decryptedMessage = Encoding.UTF8.GetString(decrypted);
+            Console.WriteLine("----------------------");
+            Console.WriteLine("DES Encryption in .NET");
+            Console.WriteLine();
+            Console.WriteLine("Original Text = " + original);
+            Console.WriteLine("Encrypted Text = " +
+            Convert.ToBase64String(encrypted));
+            Console.WriteLine("Decrypted Text = " + decryptedMessage);
+
+
+
+            var key2 = aesChipher.GenerateRandomNumber(16);
+            var iv2 = aesChipher.GenerateRandomNumber(8);
+            const string original2 = "Text to encrypt 2";
+            var encrypted2 = TripledesChipher.Encrypt(Encoding.UTF8.GetBytes(original2), key2, iv2);
+            var decrypted2 = TripledesChipher.Decrypt(encrypted2, key2, iv2);
+            var decryptedMessage2 = Encoding.UTF8.GetString(decrypted2);
+            Console.WriteLine("----------------------");
+            Console.WriteLine("TripleDes Encryption in .NET");
+            Console.WriteLine();
+            Console.WriteLine("Original Text = " + original2);
+            Console.WriteLine("Encrypted Text = " +
+            Convert.ToBase64String(encrypted2));
+            Console.WriteLine("Decrypted Text = " + decryptedMessage2);
+
+
+            var key3 = aesChipher.GenerateRandomNumber(32);
+            var iv3 = aesChipher.GenerateRandomNumber(16);
+            const string original3 = "Text to encrypt 3";
+            var encrypted3 = aesChipher.Encrypt(Encoding.UTF8.GetBytes(original3), key3, iv3);
+            var decrypted3 = aesChipher.Decrypt(encrypted3, key3, iv3);
+            var decryptedMessage3 = Encoding.UTF8.GetString(decrypted3);
+            Console.WriteLine("----------------------");
+            Console.WriteLine("AES Encryption in .NET");
+            Console.WriteLine();
+            Console.WriteLine("Original Text = " + original3);
+            Console.WriteLine("Encrypted Text = " +
+            Convert.ToBase64String(encrypted3));
+            Console.WriteLine("Decrypted Text = " + decryptedMessage3);
+
+        }
+    }
+}
+
---
+++ Lab6/Lab6.2/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Security.Cryptography;
+using System.Collections.Generic;
+
+namespace Lab6._1
+{
+    public class PBKDF2
+    {
+        public static byte[] GenerateSalt()
+        {
+            using (var randomNumberGenerator = new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[32];
+                randomNumberGenerator.GetBytes(randomNumber);
+                return randomNumber;
+            }
+        }
+        public static byte[] HashPassword(byte[] toBeHashed, byte[] salt, int numberOfRounds, System.Security.Cryptography.HashAlgorithmName hashAlgorithm, Int32 NumberOfBytes)
+        {
+            using (var rfc2898 = new Rfc2898DeriveBytes(toBeHashed, salt, numberOfRounds, hashAlgorithm))
+            {
+                return rfc2898.GetBytes(NumberOfBytes);
+            }
+        }
+    }
+    class aesChipher
+    {
+        public static byte[] Encrypt(byte[] dataToEncrypt, byte[] key, byte[] iv)
+        {
+            using (var aes = new AesCryptoServiceProvider())
+            {
+                aes.Mode = CipherMode.CBC;
+                aes.Padding = PaddingMode.PKCS7;
+                aes.Key = key;
+                aes.IV = iv;
+                using (var memoryStream = new MemoryStream())
+                {
+                    var cryptoStream = new CryptoStream(memoryStream, aes.CreateEncryptor(), CryptoStreamMode.Write);
+                    cryptoStream.Write(dataToEncrypt, 0, dataToEncrypt.Length);
+                    cryptoStream.FlushFinalBlock();
+                    return memoryStream.ToArray();
+                }
+            }
+        }
+        public static byte[] Decrypt(byte[] dataToDecrypt, byte[] key, byte[] iv)
+        {
+            using (var aes = new AesCryptoServiceProvider())
+            {
+                aes.Mode = CipherMode.CBC;
+                aes.Padding = PaddingMode.PKCS7;
+                aes.Key = key;
+                aes.IV = iv;
+                using (var memoryStream = new MemoryStream())
+                {
+                    var cryptoStream = new CryptoStream(memoryStream, aes.CreateDecryptor(), CryptoStreamMode.Write);
+                    cryptoStream.Write(dataToDecrypt, 0, dataToDecrypt.Length);
+                    cryptoStream.FlushFinalBlock();
+                    return memoryStream.ToArray();
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string original = "Text to encrypt with hash";
+            var key = PBKDF2.HashPassword(Encoding.Unicode.GetBytes(original), PBKDF2.GenerateSalt(), 30000, HashAlgorithmName.SHA256, 32);
+            var iv = PBKDF2.HashPassword(Encoding.Unicode.GetBytes(original), PBKDF2.GenerateSalt(), 30000, HashAlgorithmName.SHA256, 16);
+            var encrypted = aesChipher.Encrypt(Encoding.UTF8.GetBytes(original), key, iv);
+            var decrypted = aesChipher.Decrypt(encrypted, key, iv);
+            var decryptedMessage = Encoding.UTF8.GetString(decrypted);
+            Console.WriteLine("----------------------");
+            Console.WriteLine("AES Encryption in .NET with getting key and iv from hash");
+            Console.WriteLine();
+            Console.WriteLine("Original Text = " + original);
+            Console.WriteLine("Encrypted Text = " + Convert.ToBase64String(encrypted));
+            Console.WriteLine("Decrypted Text = " + decryptedMessage);
+        }
+    }
+}
+
